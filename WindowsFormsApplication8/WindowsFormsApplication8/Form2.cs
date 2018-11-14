a3d4ab64584f17501aadbba38989af95fb8a3bd9
elvissango <elvissango@gmail.com>
2018-11-14 14:53:30 +0200

    Add files via upload

---
+++ WindowsFormsApplication8/WindowsFormsApplication8/Form2.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication8
+{
+    public partial class CSV : Form
+    {
+        public CSV()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            ofd.Title = "Select file";
+
+            ofd.InitialDirectory = @"c:\";
+
+            //ofd.FileName = txtFileName.Text;//
+
+            ofd.Filter = "Text and CSV Files(*.txt, *.csv)|*.txt;*.csv|Text Files(*.txt)|*.txt|CSV Files(*.csv)|*.csv|All Files(*.*)|*.*";
+
+            ofd.FilterIndex = 1;
+
+            ofd.RestoreDirectory = true;
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+
+                //txtFileName.Text = ofd.FileName;//
+
+                // DataSetSchemaImporterExtension();//
+
+                Application.DoEvents();
+
+            }
+        
+        }
+    }
+}
---
+++ WindowsFormsApplication8/WindowsFormsApplication8/kkkk.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string  Usr = textBox1.Text;
+          string Pass = textBox2.Text;
+            if ((this.textBox1.Text == "Admin") && (this.textBox2.Text == "admin"))
+             // MessageBox(" Correct");//
+            
+            {
+                this.Hide();
+                CSV fr2 = new CSV();
+                fr2.ShowDialog();
+            }
+                            else 
+            {
+                MessageBox.Show("Incorrect");
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
